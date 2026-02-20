b6b82e582a58e93cd1ca838ad3adbe25ecb9d8bf
Marko Yowan <markoyowanratsimbazafy@gmail.com>
2026-02-20 20:06:07 +0300

    List is ready

--- src/features/property/page/PropertyList.tsx
+++ src/features/property/page/PropertyList.tsx
@@ -1,8 +1,10 @@
+// src/modules/property/pages/PropertyList.tsx
 import { useEffect, useState } from "react";
 import { getProperties } from "../services/PropertyService";
 import type { Property } from "../models/Property.interface";
 import PropertyCard from "../components/PropertyCard";
 import { useLoader } from "../../../shared/context/LoaderContext";
+import EmptyState from "../../../shared/components/EmptyState";
 
 const PropertyList = () => {
   const [properties, setProperties] = useState<Property[]>([]);
@@ -10,14 +12,15 @@ const PropertyList = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      showLoader(); // on affiche le loader
+      showLoader();
       try {
         const data = await getProperties();
         setProperties(data);
       } catch (err) {
         console.error(err);
+        setProperties([]); // en cas d'erreur, on vide la liste pour afficher EmptyState
       } finally {
-        hideLoader(); // on masque le loader
+        hideLoader();
       }
     };
 
@@ -26,15 +29,19 @@ const PropertyList = () => {
 
   return (
     <div className="p-8 bg-app-bg min-h-screen">
-      <h1 className="text-3xl font-bold text-text-primary mb-6">
+      <h1 className="text-4xl font-extrabold text-text-primary text-center mb-12">
         Biens publiés
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {properties.map((property) => (
-          <PropertyCard key={property.id} property={property} />
-        ))}
-      </div>
+      {properties.length === 0 ? (
+        <EmptyState message="Pas de biens disponibles pour le moment" />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {properties.map((property) => (
+            <PropertyCard key={property.id} property={property} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
